55517b10fca2dcfe06c663efb5f5c70a8f25aa6b
alcivanio <alcivaniosilva@gmail.com>
2016-11-24 19:28:28 -0300

    Adicionando a gree -

--- hello-express/index.js
+++ hello-express/index.js
@@ -6,4 +6,10 @@ let express = require("express");
 const app = express();
 
 app.get("/hello", (req, res)=>res.send("Olá mundo"));
+app.get("/greet", (req, res, next)=>{
+	if (req.query.nome) res.send(`Olá, ${req.query.nome}!`);
+	else res.send("Olá, estranho.");
+});
+
+
 app.listen(3000);
